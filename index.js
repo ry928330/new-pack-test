7347e155574bb888e4bc7fe5cb67c2439d4ff7b3
ranyu <ranyu@pwrd.com>
2022-09-26 10:29:14 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: ryyyyy
+ * @Date: 2022-09-26 10:28:10
+ * @LastEditors: ryyyyy
+ * @LastEditTime: 2022-09-26 10:28:51
+ * @FilePath: /new-pack-test/index.js
+ * @Description: 
+ * 
+ */
+const Tool = () => {
+    console.log('hello, ryyyyy');
+}
+
+module.exports = Tool;
